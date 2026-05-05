f29a581c02553fb26ef811ddd4493b7c5317432c
islombek202 <islombekkarimov246@gmail.com>
2026-05-05 16:29:33 +0500

    Add implementations for addition, subtraction, and find_max functions; update module to utilize these functions

---
+++ math_functions.py
@@ -0,0 +1,8 @@
+def addition(a, b):
+    return a + b
+def subtraction(a, b):
+    return a - b
+
+def find_max(*numbers):
+    if len(numbers)==0
+      
---
+++ modules.py
@@ -0,0 +1,3 @@
+import math_functions as mfs
+print(mfs.addition(5,3))
+print(mfs.subtraction(8,5))
